ca567e28ad66f06186016fa8088743997dc63882
luyoyu <xingctd@gmail.com>
2024-12-16 08:23:59 +0800

    添加预编译头

---
+++ src/gameworld_server/stdafx.h
@@ -0,0 +1,53 @@
+#pragma once
+// 系统库预编译头文件内容
+/*---------------------------*/
+#include <Windows.h>
+#include "shellapi.h"
+#include <algorithm>
+#include <assert.h>
+#include <atomic>
+#include <cctype>
+#include <chrono>
+#include <consoleapi2.h>
+#include <ctype.h>
+#include <errno.h>
+#include <functional>
+#include <io.h>
+#include <iomanip>
+#include <iostream>
+#include <limits.h>
+#include <list>
+#include <locale.h>
+#include <map>
+#include <math.h>
+#include <memory>
+#include <mmsystem.h>
+#include <ostream>
+#include <queue>
+#include <set>
+#include <setjmp.h>
+#include <signal.h>
+#include <sstream>
+#include <stack>
+#include <stdarg.h>
+#include <stddef.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string>
+#include <strstream>
+#include <thread>
+#include <time.h>
+#include <tlhelp32.h>
+#include <typeinfo>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+/*---------------------------*/
+
+
+
+/*---------------------------*/
+// 其他需要预编译的头文件
+#include "core/rt2_core.h"
+/*---------------------------*/
+
